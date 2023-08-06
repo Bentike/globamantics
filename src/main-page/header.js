0a5abe7a38ee6c92973d20f3428626fe79f44b3b
bentike <ajadiyusuf73@gmail.com>
2023-08-06 15:40:09 +0100

    added header component

---
+++ src/main-page/header.js
@@ -0,0 +1,15 @@
+
+const Header = () => {
+    return(
+        <header className="row">
+            <div className="col-md-5 mt-3">
+                Globamantics
+            </div>
+            <div className="col-md-7 mt-5 subtitle">
+                Providing Houses All Over The World.
+            </div>
+        </header>
+    );
+}
+
+export default Header;
