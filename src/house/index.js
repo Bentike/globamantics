459eb587c7a88ce951a1def7332524910ab53cf3
bentike <ajadiyusuf73@gmail.com>
2023-08-06 19:26:35 +0100

    fixed house bug

--- src/house/index.js
+++ src/house/index.js
@@ -1,21 +1,21 @@
 import './house.css';
 
-const House = ({house}) => {
+const House = (house) => {
     return ( 
         <div>
             <div className='row mt-2'>
-               <h5 className='col-md-12'>{house.country}</h5>
+               <h5 className='col-md-12'>{house.house.country}</h5>
             </div>
             <div className='row'>
-               <h5 className='col-md-12'>{house.address}</h5>
+               <h5 className='col-md-12'>{house.house.address}</h5>
             </div>
             <div className='row'>
                 <div className='col-md-7'>
-                   <img src={house.photo} alt='house'/>
+                   <img src={house.house.photo} alt='house'/>
                 </div>
                 <div className='col-md-5'>
-                   <p className='price'>${house.price}</p>
-                   <p>{house.description}</p>
+                   <p className='price'>${house.house.price}</p>
+                   <p>{house.house.description}</p>
                 </div>
             </div>
         </div>
--- src/main-page/featured-house.js
+++ src/main-page/featured-house.js
@@ -1,12 +1,12 @@
 import House from "../house";
 
-const FeatureHouse = ({house}) => {
+const FeatureHouse = (house) => {
     return (
         <div>
             <div className="row featuredHouse">
                 <h3 className="col-md-12 text-center">Feature House</h3>
             </div>
-            <House house={house}/>
+            <House house={house.house}/>
         </div>
     );
 }
